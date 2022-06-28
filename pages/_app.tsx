b44bad6e4ece1881c31dbcb25d71d221c7d5ce63
Otavio Tampellini <tampelliniotavio@gmail.com>
2022-06-28 12:26:40 -0300

    Primeira Implementação

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import Footer from "./components/Footer";
+import HeadTags from "./components/Head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <HeadTags />
+            <div className="flex min-h-screen flex-col items-center justify-center py-2">
+                <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+                    <Component {...pageProps} />
+                </main>
+                <Footer />
+            </div>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui:{
+    darkTheme: "dark",
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
@@ -7,5 +10,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("daisyui")
+  ],
 }
